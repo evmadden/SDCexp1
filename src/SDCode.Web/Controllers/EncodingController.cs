33c1f3088640a76c3a7812c03944081ab128e94a
evmadden <69730899+evmadden@users.noreply.github.com>
2020-09-06 20:44:12 +0100

    start building encoding tasks

---
+++ src/SDCode.Web/Controllers/EncodingController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SDCode.Web.Models;
+using SDCode.Web.Classes;
+
+namespace SDCode.Web.Controllers
+{
+    public class EncodingController : Controller
+    {
+        private readonly ILogger<EncodingController> _logger;
+
+        public EncodingController(ILogger<EncodingController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var imageUrls = new List<string>{"https://placekitten.com/800/700", ""};
+            var encodingIndexViewModel = new EncodingIndexViewModel(imageUrls);
+            return View(encodingIndexViewModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/SDCode.Web/Models/EncodingIndexViewModel.cs
@@ -0,0 +1,13 @@
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace SDCode.Web.Models
+{
+    public class EncodingIndexViewModel
+    {
+        public EncodingIndexViewModel(string IEnumerable<string> imageUrls) {
+            ImageUrls = ImageUrls;
+        }
+        IEnumerable<string> ImageUrls {get;}
+    }
+}
