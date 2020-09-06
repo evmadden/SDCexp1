96fbd67495a725cb63f6316bab0498099e7e8ed0
Lance Hilliard <lance@digitalcreations.cc>
2020-09-06 06:21:20 -0500

    store CSV booleans as numbers instead of strings

--- src/SDCode.Web/Classes/CsvFile.cs
+++ src/SDCode.Web/Classes/CsvFile.cs
@@ -5,6 +5,8 @@ using CsvHelper;
 using System.Text;
 using System.IO;
 using System.Globalization;
+using CsvHelper.TypeConversion;
+using CsvHelper.Configuration;
 
 namespace SDCode.Web.Classes
 {
@@ -16,50 +18,62 @@ namespace SDCode.Web.Classes
         (IEnumerable<byte> Bytes, string ContentType, string FileName) GetDownload();
     }
 
-public class CsvFile<T, TMap> : ICsvFile<T, TMap> where TMap : CsvHelper.Configuration.ClassMap
-{
-    public IEnumerable<T> Read()
+    public class CsvFile<T, TMap> : ICsvFile<T, TMap> where TMap : CsvHelper.Configuration.ClassMap
     {
-        IEnumerable<T> result;
-        if (File.Exists(FileName)) {
-            using (var reader = new StreamReader(FileName, Encoding.Default))
-            using (var csv = new CsvReader(reader, CultureInfo.CurrentCulture))
-            {
-                csv.Configuration.RegisterClassMap<TMap>();
-                result = csv.GetRecords<T>().ToList();
-            }
-        } else {
-            result = new List<T>();
-        }
-        return result;
-    }
-
-    public void Write(IEnumerable<T> records)
-    {
-        using (StreamWriter sw = new StreamWriter(FileName, false, new UTF8Encoding(true)))
-        using (CsvWriter cw = new CsvWriter(sw, CultureInfo.CurrentCulture))
+        public IEnumerable<T> Read()
         {
-            cw.WriteHeader<T>();
-            cw.NextRecord();
-            foreach (T record in records)
+            IEnumerable<T> result;
+            if (File.Exists(FileName)) {
+                using (var reader = new StreamReader(FileName, Encoding.Default))
+                using (var csv = new CsvReader(reader, CultureInfo.CurrentCulture))
+                {
+                    csv.Configuration.RegisterClassMap<TMap>();
+                    result = csv.GetRecords<T>().ToList();
+                }
+            } else {
+                result = new List<T>();
+            }
+            return result;
+        }
+
+        public void Write(IEnumerable<T> records)
+        {
+            using (StreamWriter sw = new StreamWriter(FileName, false, new UTF8Encoding(true)))
+            using (CsvWriter cw = new CsvWriter(sw, CultureInfo.CurrentCulture))
             {
-                cw.WriteRecord<T>(record);
+                cw.Configuration.TypeConverterCache.AddConverter<bool>(new MyBooleanConverter()); // https://stackoverflow.com/a/63523529
+                cw.WriteHeader<T>();
                 cw.NextRecord();
+                foreach (T record in records)
+                {
+                    cw.WriteRecord<T>(record);
+                    cw.NextRecord();
+                }
+            }
+        }
+
+        public (IEnumerable<byte> Bytes, string ContentType, string FileName) GetDownload() {
+            IEnumerable<byte> bytes = File.Exists(FileName) ? File.ReadAllBytes(FileName) : Enumerable.Empty<byte>();
+            return (bytes, "text/csv", FileName);
+        }
+
+        private string FileName {
+            get {
+                return $"{typeof(T).Name.Replace("Model", "Records")}.csv";
             }
         }
     }
 
-    public (IEnumerable<byte> Bytes, string ContentType, string FileName) GetDownload() {
-        IEnumerable<byte> bytes = File.Exists(FileName) ? File.ReadAllBytes(FileName) : Enumerable.Empty<byte>();
-        return (bytes, "text/csv", FileName);
-    }
-
-    private string FileName {
-        get {
-            return $"{typeof(T).Name.Replace("Model", "Records")}.csv";
+    public class MyBooleanConverter : DefaultTypeConverter
+    {
+        public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            if( value == null )
+            {
+                return string.Empty;
+            }
+            var boolValue = (bool)value;
+            return boolValue ? "1" : "0";
         }
     }
-}
-
-
 }
