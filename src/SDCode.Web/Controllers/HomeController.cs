c2ff9be68f0993580a0e35cd2f772f102a209eec
Lance Hilliard <lance@digitalcreations.cc>
2020-09-08 23:13:38 -0500

    fix #13: remove site navigation (add Learn More)

--- src/SDCode.Web/Controllers/HomeController.cs
+++ src/SDCode.Web/Controllers/HomeController.cs
@@ -105,6 +105,11 @@ namespace SDCode.Web.Controllers
             return View();
         }
 
+        public IActionResult LearnMore() 
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
