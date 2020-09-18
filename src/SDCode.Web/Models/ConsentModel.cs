cab005ef2dbe809ae9570e3b3b8333f3a5ade1ef
Lance Hilliard <lance@digitalcreations.cc>
2020-09-18 00:38:22 -0500

    #26: annotate ConsentModel

--- src/SDCode.Web/Models/ConsentModel.cs
+++ src/SDCode.Web/Models/ConsentModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using CsvHelper.Configuration;
 using CsvHelper.Configuration.Attributes;
 
@@ -6,28 +7,40 @@ namespace SDCode.Web.Models
     public class ConsentModel
     {
         [Name(nameof(ID))]
-        public string ID { get; set; }
+        [Description("ID of the participant.")]
+        public string ID { get; set; } // todo mlh ask EM if we should rename this to ParticipantID (to match every other model)
         [Name(nameof(InfoSheet))]
+        [Description("Participant has read study information sheet.")]
         public bool InfoSheet { get; set; }
         [Name(nameof(Withdraw))]
+        [Description("Participant understands withdraw freedom.")]
         public bool Withdraw { get; set; }
         [Name(nameof(NPSDisorder))]
+        [Description(@"Participant understands ""NPS history"" participation stipulation.")]
         public bool NPSDisorder{ get; set; }
         [Name(nameof(ADHD))]
+        [Description(@"Participant understands ""additional difficulties history"" participation stipulation.")]
         public bool ADHD{ get; set; } 
         [Name(nameof(HeadInjury))]
+        [Description(@"Participant understands ""serious head injury"" participation stipulation.")]
         public bool HeadInjury{ get; set; }
         [Name(nameof(NormalVision))]
+        [Description(@"Participant understands ""normal vision"" participation stipulation.")]
         public bool NormalVision{ get; set; }    
         [Name(nameof(VisionProblems))]
+        [Description(@"Participant understands ""special visual"" participation stipulation.")]
         public bool VisionProblems{ get; set; }   
         [Name(nameof(AltShifts))]
+        [Description(@"Participant understands ""alternate shift patterns"" participation stipulation.")]
         public bool AltShifts{ get; set; }
         [Name(nameof(Smoker))]
+        [Description(@"Participant understands ""smoker"" participation stipulation.")]
         public bool Smoker{ get; set; }
         [Name(nameof(DataProtection))]
+        [Description(@"Participant understands ""anonymised data access"" participation stipulation.")]
         public bool DataProtection{ get; set; }   
         [Name(nameof(AgreeParticipate))]
+        [Description("Participant agrees to take part in the study.")]
         public bool AgreeParticipate{ get; set; }            
     }
 
