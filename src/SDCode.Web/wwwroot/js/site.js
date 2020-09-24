eeece0ac5bfa3e28562664d9059d3e79cd0231a4
Lance Hilliard <lance@digitalcreations.cc>
2020-09-23 20:06:33 -0500

    refactor isInViewport to site.js

--- src/SDCode.Web/wwwroot/js/site.js
+++ src/SDCode.Web/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function isInViewport(element) { // https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
