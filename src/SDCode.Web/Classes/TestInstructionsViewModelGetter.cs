1ac28a691ceee78872e39dd88aa9cfd1da8da20c
Lance Hilliard <lance@digitalcreations.cc>
2021-07-05 16:09:57 -0500

    #73: correct test image count display

--- src/SDCode.Web/Classes/TestInstructionsViewModelGetter.cs
+++ src/SDCode.Web/Classes/TestInstructionsViewModelGetter.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using SDCode.Web.Models;
 
 namespace SDCode.Web.Classes
@@ -13,20 +14,27 @@ namespace SDCode.Web.Classes
         private readonly IJudgementsDescriptionGetter _judgementsDescriptionGetter;
         private readonly IConfidencesDescriptionsGetter _confidencesDescriptionsGetter;
         private readonly IPhaseSetsGetter _phaseSetsGetter;
+        private readonly IProgressGetter _progressGetter;
+        private readonly ITestNameGetter _testNameGetter;
 
-        public TestInstructionsViewModelGetter(IJudgementsDescriptionGetter judgementsDescriptionGetter, IConfidencesDescriptionsGetter confidencesDescriptionsGetter, IPhaseSetsGetter phaseSetsGetter)
+        public TestInstructionsViewModelGetter(IJudgementsDescriptionGetter judgementsDescriptionGetter, IConfidencesDescriptionsGetter confidencesDescriptionsGetter, IPhaseSetsGetter phaseSetsGetter, IProgressGetter progressGetter, ITestNameGetter testNameGetter)
         {
             _judgementsDescriptionGetter = judgementsDescriptionGetter;
             _confidencesDescriptionsGetter = confidencesDescriptionsGetter;
             _phaseSetsGetter = phaseSetsGetter;
+            _progressGetter = progressGetter;
+            _testNameGetter = testNameGetter;
         }
 
         public TestInstructionsViewModel Get(string participantID)
         {
+            var phaseSets = _phaseSetsGetter.Get(participantID);
+            var progress = _progressGetter.Get(participantID);
+            var testName = _testNameGetter.Get(phaseSets, progress);
             var oldJudgementsDescription = _judgementsDescriptionGetter.Get(Judgements.Old);
             var newJudgementsDescription = _judgementsDescriptionGetter.Get(Judgements.New);
             var confidenceDescriptions = _confidencesDescriptionsGetter.Get().ToDictionary(x=>$"{x.Key}", x=>x.Value);
-            var stimuliCount = _phaseSetsGetter.Get(participantID).EncodingCount;
+            var stimuliCount = Convert.ToInt32(phaseSets.GetType().GetProperty($"{testName}Count").GetValue(phaseSets, null));
             var result = new TestInstructionsViewModel(oldJudgementsDescription, newJudgementsDescription, confidenceDescriptions, stimuliCount);
             return result;
         }
